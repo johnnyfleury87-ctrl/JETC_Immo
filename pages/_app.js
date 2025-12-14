120a854c42c14296f99d6402ce38a72887be01b9
johnnyfleury87-ctrl <johnny.fleury87@gmail.com>
2025-12-14 20:58:38 +0000

    login

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import { ThemeProvider } from "../context/ThemeContext";
 import { DemoModeProvider } from "../context/DemoModeContext";
+import { createClient } from "@supabase/supabase-js";
 import "../styles/global.css";
 import "../styles/animations.css";
 import "../styles/marketing.css";
@@ -7,7 +10,34 @@ import "../styles/theme-speciale.css";
 import "../styles/theme-jardin.css";
 import "../styles/theme-zen.css";
 
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
+
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleMagicLinkReturn = async () => {
+      const { data: { session } } = await supabase.auth.getSession();
+
+      if (session?.user) {
+        const { data: profile } = await supabase
+          .from('profiles')
+          .select('role')
+          .eq('id', session.user.id)
+          .single();
+
+        if (profile?.role === 'admin_jtec' && router.pathname !== '/admin/jetc') {
+          router.replace('/admin/jetc');
+        }
+      }
+    };
+
+    handleMagicLinkReturn();
+  }, [router.pathname]);
+
   return (
     <DemoModeProvider>
       <ThemeProvider>
